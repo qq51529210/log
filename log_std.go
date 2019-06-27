01a9c6367ca438db6c69527aa911965d86858626
ben <51529210@qq.com>
2019-06-27 10:40:58 +0800

    u

--- log_std.go
+++ log_std.go
@@ -9,14 +9,13 @@ type LoggerStd struct {
 	fileLine FileLine
 }
 
-// skip 一般是3
+// 某个Package里使用并导出了std.Logger
+// 设置std.Logger = NewLoggerStd(server.logger, log.LevelError, 3, log.FileLineFullPath), "", 0)
+// 一般Package不会直接使用std.Logger.Output()
+// 所以skip一般是3，就可以定位到具体的行
+// LoggerStd实现了io.Writer，可以接收std.Logger的输出
 func NewLoggerStd(writer io.Writer, level Level, skip int, fileLine FileLine) *LoggerStd {
-	return &LoggerStd{
-		writer:   writer,
-		level:    level,
-		skip:     skip,
-		fileLine: fileLine,
-	}
+	return &LoggerStd{writer: writer, level: level, skip: skip, fileLine: fileLine}
 }
 
 func (this *LoggerStd) Write(b []byte) (int, error) {
